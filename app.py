2238a4c2778d0d437b449ece1266e37dbc07dd8a
Wing-Dimas <wingdimas@gmail.com>
2022-11-13 17:18:56 +0700

    initial awal

---
+++ app.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import pickle
+import streamlit as st
+import plotly.express as px
+import pickle
+import numpy as np
+
+px.defaults.template = "plotly_dark"
+px.defaults.color_continuous_scale = "reds"
+
+st.sidebar.title("Input Data")
+# read data
+data = pd.read_csv("https://raw.githubusercontent.com/Wing-Dimas/datamining/main/credit_score.csv")
+st.dataframe(data)
+
+# create input
+pendapatan_setahun = st.sidebar.text_input("Pendapatan Setahun(juta)")
+kpr = st.sidebar.radio("KPR", ("aktif", "tidak aktif"))
+jumlah_tanggungan = st.sidebar.text_input("Jumlah Tanggungan")
+durasi_pinjaman = st.sidebar.selectbox("Durasi (bulan)", ("12", "36", "48"))
+overdue = st.sidebar.selectbox("Overdue", ("0 - 30 days", "31 - 45 days", "46 - 60 days", "61 - 90 days", "> 90 days"))
+
+# create content
+st.title("Credit Score")
+st.header("Data Sample")
+
+
+st.header("Hasil")
+def submit():
+    with open("model.sav", "rb") as model_buffer:
+        # cek input 
+        kpr_ya = 0
+        kpr_tidak = 0
+        if kpr == "aktif":
+            kpr_ya = 1
+        else:
+            kpr_tidak = 1
+
+
+        overdues = [0,0,0,0,0]
+        if(overdue == "0 - 30 days"):
+            overdues[0] = 1
+        elif(overdue == "31 - 45 days"):
+            overdues[1] = 1
+        elif(overdue == "46 - 60 days"):
+            overdues[2] = 1
+        elif(overdue == "61 - 90 days"):
+            overdues[3] = 1
+        else:
+            overdues[4] = 1
+
+        # create data input
+        data_input = {
+            "pendapatan_setahun_juta" : int(pendapatan_setahun),
+            "durasi_pinjaman_bulan" : int(durasi_pinjaman),
+            "jumlah_tanggungan" : int(jumlah_tanggungan),
+            "overdue_0 - 30 days": overdues[0],
+            "overdue_31 - 45 days": overdues[1],
+            "overdue_46 - 60 days": overdues[2],
+            "overdue_61 - 90 days": overdues[3],
+            "overdue_> 90 days": overdues[4],
+            "KPR_TIDAK" : kpr_tidak,
+            "KPR_YA": kpr_ya
+        }
+
+        inputs = np.array([[val for val in data_input.values()]])
+
+        model = pickle.load(model_buffer)
+        pred = model.predict(inputs)
+        st.text(pred)
+
+
+submitted = st.sidebar.button("Submit")
+if submitted:
+    submit()
